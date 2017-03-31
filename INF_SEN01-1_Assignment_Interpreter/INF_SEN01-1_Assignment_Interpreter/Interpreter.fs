370c680cea37b54cc9a8942144b611e840aa8107
swenmeeuwes <swenmeeu@gmail.com>
2017-03-31 11:30:43 +0200

    Added: divide and multiply expression

--- INF_SEN01-1_Assignment_Interpreter/INF_SEN01-1_Assignment_Interpreter/Interpreter.fs
+++ INF_SEN01-1_Assignment_Interpreter/INF_SEN01-1_Assignment_Interpreter/Interpreter.fs
@@ -7,14 +7,30 @@ exception Exception of string
 let rec eval expression =
     match expression with
             | Value v -> v
+            | Multiply (left, right) -> 
+                (fun left right -> 
+                    match left, right with
+                    | IntegerType n1, IntegerType n2    -> IntegerType (n1 * n2)
+                    | FloatType n1, FloatType n2        -> FloatType (n1 * n2)
+                    | DoubleType n1, DoubleType n2      -> DoubleType (n1 * n2)                    
+                    | _                                 -> raise (Exception "Cannot multiply, incompatible types.")
+                ) (eval left) (eval right)
+            | Divide (left, right) -> 
+                (fun left right -> 
+                    match left, right with
+                    | IntegerType n1, IntegerType n2    -> IntegerType (n1 / n2)
+                    | FloatType n1, FloatType n2        -> FloatType (n1 / n2)
+                    | DoubleType n1, DoubleType n2      -> DoubleType (n1 / n2)                    
+                    | _                                 -> raise (Exception "Cannot divide, incompatible types.")
+                ) (eval left) (eval right)
             | Add (left, right) -> 
                 (fun left right -> 
                     match left, right with
                     | IntegerType n1, IntegerType n2    -> IntegerType (n1 + n2)
                     | FloatType n1, FloatType n2        -> FloatType (n1 + n2)
                     | DoubleType n1, DoubleType n2      -> DoubleType (n1 + n2)
-                    | StringType n1, StringType n2      -> StringType (n1 + n2)
-                    | _                         -> raise (Exception "Cannot sum, incompatible types.")
+                    | StringType n1, StringType n2      -> StringType (n1 + n2) // Concatenation
+                    | _                                 -> raise (Exception "Cannot sum, incompatible types.")
                 ) (eval left) (eval right)
             | Subtract (left, right) -> 
                 (fun left right -> 
@@ -22,6 +38,9 @@ let rec eval expression =
                     | IntegerType n1, IntegerType n2    -> IntegerType (n1 - n2)
                     | FloatType n1, FloatType n2        -> FloatType (n1 - n2)
                     | DoubleType n1, DoubleType n2      -> DoubleType (n1 - n2)                    
-                    | _                         -> raise (Exception "Cannot sum, incompatible types.")
+                    | _                                 -> raise (Exception "Cannot subtract, incompatible types.")
                 ) (eval left) (eval right)
+            
+            
+
             // Add default case -> raise exception
