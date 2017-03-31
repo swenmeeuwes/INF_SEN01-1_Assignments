f3a94dcba5446973717c38482773a7b1fffd0494
swenmeeuwes <swenmeeu@gmail.com>
2017-03-31 11:55:34 +0200

    Added float to int helper for possible future implementations

--- INF_SEN01-1_Assignment_Interpreter/INF_SEN01-1_Assignment_Interpreter/Interpreter.fs
+++ INF_SEN01-1_Assignment_Interpreter/INF_SEN01-1_Assignment_Interpreter/Interpreter.fs
@@ -1,46 +1,50 @@
 ﻿module Interpreter
 
+open System
 open Syntax
 
 exception Exception of string
 
+// Helpers
+let floatToInt subject = int(System.Math.Round(subject: float))
+
+// Evaluation
 let rec eval expression =
     match expression with
-            | Value v -> v
-            | Multiply (left, right) -> 
-                (fun left right -> 
-                    match left, right with
-                    | IntegerType n1, IntegerType n2    -> IntegerType (n1 * n2)
-                    | FloatType n1, FloatType n2        -> FloatType (n1 * n2)
-                    | DoubleType n1, DoubleType n2      -> DoubleType (n1 * n2)                    
-                    | _                                 -> raise (Exception "Cannot multiply, incompatible types.")
-                ) (eval left) (eval right)
-            | Divide (left, right) -> 
-                (fun left right -> 
-                    match left, right with
-                    | IntegerType n1, IntegerType n2    -> IntegerType (n1 / n2)
-                    | FloatType n1, FloatType n2        -> FloatType (n1 / n2)
-                    | DoubleType n1, DoubleType n2      -> DoubleType (n1 / n2)                    
-                    | _                                 -> raise (Exception "Cannot divide, incompatible types.")
-                ) (eval left) (eval right)
-            | Add (left, right) -> 
-                (fun left right -> 
-                    match left, right with
-                    | IntegerType n1, IntegerType n2    -> IntegerType (n1 + n2)
-                    | FloatType n1, FloatType n2        -> FloatType (n1 + n2)
-                    | DoubleType n1, DoubleType n2      -> DoubleType (n1 + n2)
-                    | StringType n1, StringType n2      -> StringType (n1 + n2) // Concatenation
-                    | _                                 -> raise (Exception "Cannot sum, incompatible types.")
-                ) (eval left) (eval right)
-            | Subtract (left, right) -> 
-                (fun left right -> 
-                    match left, right with
-                    | IntegerType n1, IntegerType n2    -> IntegerType (n1 - n2)
-                    | FloatType n1, FloatType n2        -> FloatType (n1 - n2)
-                    | DoubleType n1, DoubleType n2      -> DoubleType (n1 - n2)                    
-                    | _                                 -> raise (Exception "Cannot subtract, incompatible types.")
-                ) (eval left) (eval right)
-            
-            
+    | Value v -> v
+    | Multiply (left, right) -> 
+        (fun left right -> 
+            match left, right with
+            | IntegerType n1, IntegerType n2    -> IntegerType (n1 * n2)
+            | FloatType n1, FloatType n2        -> FloatType (n1 * n2)
+            | DoubleType n1, DoubleType n2      -> DoubleType (n1 * n2)                    
+            | _                                 -> raise (Exception "Cannot multiply, incompatible types.")
+        ) (eval left) (eval right)
+    | Divide (left, right) -> 
+        (fun left right -> 
+            match left, right with
+            | IntegerType n1, IntegerType n2    -> IntegerType (n1 / n2)
+            | FloatType n1, FloatType n2        -> FloatType (n1 / n2)
+            | DoubleType n1, DoubleType n2      -> DoubleType (n1 / n2)                    
+            | _                                 -> raise (Exception "Cannot divide, incompatible types.")
+        ) (eval left) (eval right)
+    | Add (left, right) -> 
+        (fun left right -> 
+            match left, right with
+            | IntegerType n1, IntegerType n2    -> IntegerType (n1 + n2)
+            | FloatType n1, FloatType n2        -> FloatType (n1 + n2)
+            | DoubleType n1, DoubleType n2      -> DoubleType (n1 + n2)
+            | StringType n1, StringType n2      -> StringType (n1 + n2) // Concatenation
+            | _                                 -> raise (Exception "Cannot sum, incompatible types.")
+        ) (eval left) (eval right)
+    | Subtract (left, right) -> 
+        (fun left right -> 
+            match left, right with
+            | IntegerType n1, IntegerType n2    -> IntegerType (n1 - n2)
+            | FloatType n1, FloatType n2        -> FloatType (n1 - n2)
+            | DoubleType n1, DoubleType n2      -> DoubleType (n1 - n2)
+            | _                                 -> raise (Exception "Cannot subtract, incompatible types.")
+        ) (eval left) (eval right)
 
             // Add default case -> raise exception
+            // Not needed since all Expression cases are handled
