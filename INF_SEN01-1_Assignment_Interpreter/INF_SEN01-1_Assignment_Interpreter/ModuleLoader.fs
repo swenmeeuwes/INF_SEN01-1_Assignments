035493844c96531f175e716e39f3e29c37b38a38
swenmeeuwes <swenmeeu@gmail.com>
2017-04-13 16:14:39 +0200

    Moved development files to its own branch, so that the master branch is always ready for deadlines

---
+++ INF_SEN01-1_Assignment_Interpreter/INF_SEN01-1_Assignment_Interpreter/ModuleLoader.fs
@@ -0,0 +1,10 @@
+﻿module ModuleLoader
+
+open System
+open System.IO
+
+let loadModule path =
+    File.ReadAllText(path)
+
+let interpretModule path delimiter =
+    (loadModule path).Split delimiter
---
+++ INF_SEN01-1_Assignment_Interpreter/INF_SEN01-1_Assignment_Interpreter/Parser.fs
@@ -0,0 +1,20 @@
+﻿module Parser
+
+open Syntax
+open System
+
+exception Exception of string
+
+let (|Int|_|) str =
+   match System.Int32.TryParse(str) with
+   | (true,int) -> Some(int)
+   | _ -> None
+
+let parseToken (tokenString: string) =
+    match tokenString with
+    | Int x -> IntegerType(x)
+    | _     -> raise (Exception "")
+
+let parseTokenSeq prevToken currToken nextToken =
+    match currToken with
+    | "+" -> Add(Value (parseToken prevToken), Value (parseToken nextToken))
